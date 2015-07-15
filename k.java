bfc51e743fb5539aa727a012dba8c1451e937bcf
root-sudip <touch.das@gmail.com>
2015-07-16 00:43:39 +0530

    newhe06

--- k.java
+++ k.java
@@ -41,7 +41,7 @@ JMenuItem save = new JMenuItem("Save");
 JMenuItem exit = new JMenuItem("Exit");
 
 
-asds
+as
 CaretListener ctlistener = new CaretListener(){
     public void caretUpdate(CaretEvent cl){
         Highlighter highlight = textArea.getHighlighter();
